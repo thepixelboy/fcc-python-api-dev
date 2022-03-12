7075985d9f0162c00fc009c891beb891d40a36f5
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-12 18:55:55 +0100

    feat: connecting to database with python

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
+import os
+import time
 from random import randrange
 from typing import Optional
 
+import psycopg2
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Response, status
+from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
+
+load_dotenv()
+
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_name = os.environ.get("DB_NAME")
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+
 
 app = FastAPI()
 
@@ -12,6 +26,25 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_pass,
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Successfully connected to the database")
+        break
+    except Exception as error:
+        print("Failed to connect to database")
+        print("Error: ", error)
+        time.sleep(4)
 
 
 my_posts = [
