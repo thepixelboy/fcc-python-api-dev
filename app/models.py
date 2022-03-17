c262a595d49e7437eb81c7e5b2f60756163d19db
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-17 19:03:39 +0100

    feat: add created_at column

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
@@ -9,4 +11,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
