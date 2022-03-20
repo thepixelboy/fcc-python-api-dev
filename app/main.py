e5bb0d8dc43172ac3392d63fb8f7ac674ea5c442
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-20 17:40:18 +0100

    feat: create post

--- app/main.py
+++ app/main.py
@@ -88,17 +88,14 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute(
-        """
-            INSERT INTO posts (title, content, published) 
-            VALUES (%s, %s, %s) 
-            RETURNING * 
-        """,
-        (post.title, post.content, post.published),
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(
+        title=post.title, content=post.content, published=post.published
     )
-    new_post = cursor.fetchone()
-    conn.commit()
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
     return {"data": new_post}
 
 
