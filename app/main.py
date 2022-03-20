2195dc47c5e5344bf3ec1bd5494817e061db1eb4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-20 17:49:46 +0100

    refactor: pass all post field - value pairs as **kwarg

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import os
 import time
-from typing import Optional
 
 import psycopg2
 from dotenv import load_dotenv
@@ -30,7 +29,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 
 while True:
@@ -89,9 +87,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session = Depends(get_db)):
-    new_post = models.Post(
-        title=post.title, content=post.content, published=post.published
-    )
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
