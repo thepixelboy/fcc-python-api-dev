8c5eec10bd67178eb6ef574ff19ead37bc08be52
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-20 18:03:31 +0100

    feat: delete post

--- app/main.py
+++ app/main.py
@@ -109,18 +109,17 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute(
-        """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id))
-    )
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post is None:
+    if post.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id {id} not found",
         )
+
+    post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
