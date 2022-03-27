8d40ed1a5294e95bdb89a852050bca904a492057
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-03-27 17:47:09 +0200

    feat: fastapi routers

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 import os
 import time
-from typing import List
 
 import psycopg2
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, HTTPException, Response, status
+from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
-from sqlalchemy.orm import Session
 
-from . import models, schemas, utils
-from .database import engine, get_db
+from . import models
+from .database import engine
+from .routers import post, user
 
 load_dotenv()
 
@@ -24,6 +23,8 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+app.include_router(post.router)
+app.include_router(user.router)
 
 
 while True:
@@ -45,123 +46,6 @@ while True:
         time.sleep(4)
 
 
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "favorite foods", "content": "I like pizza", "id": 2},
-]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
-
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
-
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-@app.post(
-    "/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post
-)
-def create_posts(post: schemas.CreatePost, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, response: Response, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id {id} not found",
-        )
-
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id {id} not found",
-        )
-
-    post.delete(synchronize_session=False)
-    db.commit()
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put(
-    "/posts/{id}",
-    status_code=status.HTTP_202_ACCEPTED,
-    response_model=schemas.Post,
-)
-def update_post(
-    id: int, post: schemas.CreatePost, db: Session = Depends(get_db)
-):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post_to_update = post_query.first()
-
-    if post_to_update is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id {id} not found",
-        )
-
-    post_query.update(post.dict(), synchronize_session=False)
-    db.commit()
-
-    return post_query.first()
-
-
-@app.post(
-    "/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User
-)
-def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
-    # hash the password - user.password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-
-@app.get("/users/{id}", response_model=schemas.User)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id {id} does not exist",
-        )
-
-    return user
