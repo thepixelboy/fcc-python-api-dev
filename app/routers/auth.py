3cc488eeaba1ead48606cbd93cb0c1c31bbb1985
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-04-03 13:03:40 +0200

    refactor: oauth2 passwordrequestform

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from .. import database, models, oauth2, schemas, utils
+from .. import database, models, oauth2, utils
 
 router = APIRouter(prefix="/login", tags=["Authentication"])
 
 
 @router.post("/")
 def login(
-    user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
 ):
     user = (
         db.query(models.User)
-        .filter(models.User.email == user_credentials.email)
+        .filter(models.User.email == user_credentials.username)
         .first()
     )
 
