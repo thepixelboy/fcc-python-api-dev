afcb49466c1767d0b3722ec91e085b8c04c51441
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-05-06 19:21:43 +0200

    feat: votes route

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel, EmailStr
+from pydantic.types import conint
 
 
 class PostBase(BaseModel):
@@ -50,3 +51,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)  # type: ignore
