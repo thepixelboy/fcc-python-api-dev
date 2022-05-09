fa454364f86449305279966e5905770fe1247826
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-05-09 13:21:49 +0200

    feat: get individual posts by id using joins

--- app/routers/post.py
+++ app/routers/post.py
@@ -58,7 +58,7 @@ def create_posts(
     return new_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOutput)
 def get_post(
     id: int,
     response: Response,
@@ -66,7 +66,14 @@ def get_post(
     current_user: int = Depends(oauth2.get_current_user),
 ):
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.id == id)
+        .first()
+    )
 
     if not post:
         raise HTTPException(
