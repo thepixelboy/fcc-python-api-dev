e9b6f5e2348a8dc30c343ec04a5007779f07441e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-05-16 14:06:47 +0200

    test: create and destroy database after each test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,10 @@
+import pytest
 from app import schemas
 from app.config import settings
 from app.database import Base, get_db
 from app.main import app
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
@@ -14,10 +14,6 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
 )
-
-Base.metadata.create_all(bind=engine)
-
-# Base = declarative_base()
 
 
 def override_get_db():
@@ -31,16 +27,20 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
 
 
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Welcome to my API"
     assert res.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post(
         "/users/",
         json={"email": "test@example.com", "password": "password123"},
