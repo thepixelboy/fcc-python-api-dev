41d26343c61df326a38e6b6ee2ff948f525c4157
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-05-16 12:05:15 +0200

    test: test create_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+from app import schemas
 from app.main import app
 from fastapi.testclient import TestClient
 
@@ -8,3 +9,14 @@ def test_root():
     res = client.get("/")
     assert res.json().get("message") == "Welcome to my API"
     assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post(
+        "/users/",
+        json={"email": "test@example.com", "password": "password123"},
+    )
+
+    new_user = schemas.User(**res.json())
+    assert new_user.email == "test@example.com"
+    assert res.status_code == 201
