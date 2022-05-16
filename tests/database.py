54c00cda42de04423c7a0987f98e2bab3a1d8440
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-05-16 14:42:31 +0200

    test: more fixtures to handle database interaction

---
+++ tests/database.py
@@ -0,0 +1,38 @@
+import pytest
+from app.config import settings
+from app.database import Base, get_db
+from app.main import app
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,46 +1,15 @@
-import pytest
 from app import schemas
-from app.config import settings
-from app.database import Base, get_db
-from app.main import app
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)
+from .database import client, session
 
 
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
-
-
-def test_root(client):
+def test_root(client, session):
     res = client.get("/")
     assert res.json().get("message") == "Welcome to my API"
     assert res.status_code == 200
 
 
-def test_create_user(client):
+def test_create_user(client, session):
     res = client.post(
         "/users/",
         json={"email": "test@example.com", "password": "password123"},
